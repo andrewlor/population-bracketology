fa11fa8c2276d28dbdac69eba2f4e13c7d783ce6
andrewlor <andrewlor332@gmail.com>
2018-03-24 01:20:30 -0400

    add hardcoded world data

---
+++ src/dataProcessor.js
@@ -0,0 +1,267 @@
+let data = {
+	"AFRICA": "1250",
+	"Algeria": "42.2",
+	"Egypt": "93.4",
+	"Libya": "6.4",
+	"Morocco": "35.1",
+	"Sudan": "40.6",
+	"Tunisia": "11.5",
+	"Western Sahara": "0.6",
+	"Benin": "11.2",
+	"Burkina Faso": "19.6",
+	"Cape Verde": "0.5",
+	"Cote d'Ivoire": "24.4",
+	"Gambia": "2.1",
+	"Ghana": "28.8",
+	"Guinea": "11.5",
+	"Guinea-Bissau": "1.9",
+	"Liberia": "4.7",
+	"Mali": "18.9",
+	"Mauritania": "4.4",
+	"Niger": "20.6",
+	"Nigeria": "190.9",
+	"Senegal": "15.8",
+	"Sierra Leone": "7.6",
+	"Togo": "7.8",
+	"Burundi": "10.4",
+	"Comoros": "0.8",
+	"Djibouti": "1",
+	"Eritrea": "5.9",
+	"Ethiopia": "105",
+	"Kenya": "49.7",
+	"Madagascar": "25.5",
+	"Malawi": "18.6",
+	"Mauritius": "1.3",
+	"Mozambique": "29.7",
+	"Rwanda": "12.3",
+	"Seychelles": "0.09",
+	"Somalia": "14.7",
+	"South Sudan": "12.6",
+	"Tanzania": "57.5",
+	"Uganda": "42.8",
+	"Zambia": "16.4",
+	"Zimbabwe": "16.6",
+	"Angola": "28.6",
+	"Cameroon": "25",
+	"Central African Republic": "4.7",
+	"Chad": "14.9",
+	"Congo": "5",
+	"Congo:  Dem. Rep.": "81.5",
+	"Equatorial Guinea": "1.3",
+	"Gabon": "2",
+	"Sao Tome and Principe": "0.2",
+	"Botswana": "2.3",
+	"Lesotho": "2.2",
+	"Namibia": "2.5",
+	"South Africa": "56.5",
+	"Swaziland": "1.4",
+	"NORTHERN AMERICA": "362",
+	"Canada": "36.7",
+	"United States": "325.4",
+	"LATIN AMERICA AND THE CARIBBEAN": "643",
+	"Belize": "0.4",
+	"Costa Rica": "4.9",
+	"El Salvador": "6.4",
+	"Guatemala": "16.9",
+	"Honduras": "8.9",
+	"Mexico": "129.2",
+	"Nicaragua": "6.2",
+	"Panama": "4.1",
+	"Antigua and Barbuda": "0.1",
+	"Bahamas": "0.4",
+	"Barbados": "0.3",
+	"Cuba": "11.3",
+	"Curacao": "0.2",
+	"Dominica": "0.07",
+	"Dominican Republic": "10.7",
+	"Grenada": "0.1",
+	"Haiti": "10.6",
+	"Jamaica": "2.9",
+	"Puerto Rico": "3.4",
+	"St. Kitts-Nevis": "0.05",
+	"Saint Lucia": "0.2",
+	"St. Vincent and the Grenadines": "0.1",
+	"Trinidad and Tobago": "1.4",
+	"Argentina": "44.3",
+	"Bolivia": "11.1",
+	"Brazil": "207.9",
+	"Chile": "18.4",
+	"Colombia": "49.3",
+	"Ecuador": "16.8",
+	"Guyana": "0.8",
+	"Paraguay": "6.8",
+	"Peru": "31.8",
+	"Suriname": "0.6",
+	"Uruguay": "3.5",
+	"Venezuela": "31.4",
+	"ASIA": "4: 494",
+	"Armenia": "3",
+	"Azerbaijan": "9.9",
+	"Bahrain": "1.5",
+	"Cyprus": "1.2",
+	"Georgia": "3.9",
+	"Iraq": "39.2",
+	"Israel": "8.3",
+	"Jordan": "9.7",
+	"Kuwait": "4.1",
+	"Lebanon": "6.2",
+	"Oman": "4.7",
+	"Qatar": "2.7",
+	"Saudi Arabia": "32.6",
+	"Syria": "18.3",
+	"Turkey": "80.9",
+	"United Arab Emirates": "9.4",
+	"Yemen": "28.3",
+	"Kazakhstan": "18",
+	"Kyrgyzstan": "6.2",
+	"Tajikistan": "8.8",
+	"Turkmenistan": "5.8",
+	"Uzbekistan": "32.4",
+	"Afghanistan": "35.5",
+	"Bangladesh": "164.7",
+	"Bhutan": "0.8",
+	"India": "1: 352.6",
+	"Iran": "80.6",
+	"Maldives": "0.4",
+	"Nepal": "29.4",
+	"Pakistan": "199.3",
+	"Sri Lanka": "21.4",
+	"Brunei": "0.4",
+	"Cambodia": "15.9",
+	"Indonesia": "264",
+	"Laos": "7",
+	"Malaysia": "31.6",
+	"Myanmar": "53.4",
+	"Philippines": "105",
+	"Singapore": "5.7",
+	"Thailand": "66.1",
+	"Timor-Leste": "1.3",
+	"Vietnam": "93.7",
+	"China": "1: 386.8",
+	"Japan": "126.7",
+	"Korea:  North": "25.5",
+	"Korea:  South": "51.4",
+	"Mongolia": "3.2",
+	"Taiwan": "23.6",
+	"EUROPE": "745",
+	"Denmark": "5.8",
+	"Estonia": "1.3",
+	"Finland": "5.5",
+	"Iceland": "0.3",
+	"Ireland": "4.8",
+	"Latvia": "1.9",
+	"Lithuania": "2.8",
+	"Norway": "5.3",
+	"Sweden": "10.1",
+	"United Kingdom": "66.2",
+	"Austria": "8.8",
+	"Belgium": "11.3",
+	"France": "65",
+	"Germany": "83.1",
+	"Liechtenstein": "0.04",
+	"Luxembourg": "0.6",
+	"Monaco": "0.04",
+	"Netherlands": "17.1",
+	"Switzerland": "8.5",
+	"Belarus": "9.5",
+	"Bulgaria": "7.1",
+	"Czech Republic": "10.6",
+	"Hungary": "9.8",
+	"Moldova": "3.6",
+	"Poland": "38.4",
+	"Romania": "19.6",
+	"Russia": "146.8",
+	"Slovakia": "5.4",
+	"Ukraine": "42.3",
+	"Albania": "2.9",
+	"Andorra": "0.08",
+	"Bosnia-Herzegovina": "3.5",
+	"Croatia": "4.1",
+	"Greece": "10.7",
+	"Italy": "60.5",
+	"Kosovo": "1.8",
+	"Macedonia": "2.1",
+	"Malta": "0.4",
+	"Montenegro": "0.6",
+	"Portugal": "10.3",
+	"San Marino": "0.03",
+	"Serbia": "7",
+	"Slovenia": "2.1",
+	"Spain": "46.6",
+	"OCEANIA": "42",
+	"Australia": "24.5",
+	"Federated States of Micronesia": "0.1",
+	"Fiji": "0.9",
+	"French Polynesia": "0.3",
+	"Guam": "0.2",
+	"Kiribati": "0.1",
+	"Marshall Islands": "0.06",
+	"Nauru": "0.01",
+	"New Caledonia": "0.3",
+	"New Zealand": "4.8",
+	"Palau": "0.02",
+	"Papua New Guinea": "8.3",
+	"Samoa": "0.2",
+	"Solomon Islands": "0.7",
+	"Tonga": "0.1",
+	"Tuvalu": "0.01",
+	"Vanuatu": "0.3"
+}
+
+function randomProperty(obj) {
+  var keys = Object.keys(obj)
+  let key = keys[ keys.length * Math.random() << 0]
+  return {key: key, value: obj[key]};
+}
+
+function dataProcessor(size) {
+	let arr = []
+	for (let i = 0; i < size; ++i) arr.push(randomProperty(data))
+	let arr1 = []
+	for (let i = 0; i < size; i += 2) {
+		let obj = {left: arr[i].key, right: arr[i+1].key}
+		if (Number(arr[i].value) < Number(arr[i+1].value)) {
+			obj.correct = arr[i+1].key
+		} else {
+			obj.correct = arr[i].key
+		}
+		arr1.push(obj)
+	}
+
+	while (arr1.length > 1) {
+		let tempArr = []
+		for (let i = 0; i < arr1.length; i += 2) {
+			let obj = {left: arr1[i], right: arr1[i+1]}
+			if (Number(data[arr1[i].correct]) < Number(data[arr1[i+1].correct])) {
+				obj.correct = arr1[i+1].correct
+			} else {
+				obj.correct = arr1[i].correct
+			}
+			tempArr.push(obj)
+		}
+		arr1 = tempArr
+	}
+
+	return arr1[0]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default dataProcessor
--- src/index.js
+++ src/index.js
@@ -3,37 +3,7 @@ import ReactDOM from 'react-dom';
 import './css/main.css';
 import Bracketology from './components/Bracketology';
 import registerServiceWorker from './registerServiceWorker';
+import getData from './dataProcessor';
 
-ReactDOM.render(<Bracketology data={
-	{
-		text: "root", 
-		left: {
-			left: {
-				left: "Calgary",
-				right: "Edmonton",
-				correct: "Calgary"
-			},
-			right: {
-				left: "Toronto",
-				right: "Montreal",
-				correct: "Toronto"
-			},
-			correct: "Toronto"
-		},
-		right: {
-			left: {
-				left: "Vancouver",
-				right: "Victoria",
-				correct: "Vancouver"
-			},
-			right: {
-				left: "Regina",
-				right: "Winnipeg",
-				correct: "Winnipeg"
-			},
-			correct: "Vancouver"
-		},
-		correct: "Toronto"
-	}
-} />, document.getElementById('root'));
+ReactDOM.render(<Bracketology data={getData(16)} />, document.getElementById('root'));
 registerServiceWorker();
